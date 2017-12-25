00aec192004a36a360da3e8fda68bd795468f34b
Relfos <relfos@gmail.com>
2017-12-25 16:38:47 +0000

    Added Unit Test demo

---
+++ ICO-Unit-Tests/ContractTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using LunarParser;
+using LunarParser.JSON;
+using Neo.Emulator;
+using NUnit.Framework;
+
+namespace ICO_Unit_Tests
+{
+    [TestFixture]
+    public class ContractTests
+    {
+        private static NeoEmulator emulator; 
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var path = TestContext.CurrentContext.TestDirectory.Replace("ICO-Unit-Tests", "ICO-Template");
+            Directory.SetCurrentDirectory(path);
+            var avmBytes = File.ReadAllBytes("ICOContract.avm");
+            emulator = new NeoEmulator(avmBytes);
+        }
+
+        [Test]
+        public void TestSymbol()
+        {
+            var inputs = DataNode.CreateArray();
+            inputs.AddValue("symbol");
+            inputs.AddValue(null);
+
+            emulator.LoadInputs(inputs);
+            emulator.Reset();
+
+            emulator.Run();
+
+            var result = emulator.GetResult();
+            Assert.NotNull(result);
+
+            var symbol = result.GetString();
+            Assert.IsTrue(symbol.Equals("DEMO"));
+        }
+    }   
+}
