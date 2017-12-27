913d25198804bba3e62e8ff75b94e01180a7ae6f
Relfos <relfos@gmail.com>
2017-12-27 21:24:20 +0000

    Merged VM into Emulator project

--- NEO-Emulator/Helper.cs
+++ NEO-Emulator/Helper.cs
@@ -3,6 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
 
 namespace Neo.Emulator
 {
@@ -72,6 +76,281 @@ namespace Neo.Emulator
             byte[] ret = new byte[count];
             Buffer.BlockCopy(source, 0, ret, 0, count);
             return ret;
+        }
+
+        private static readonly DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static ScriptBuilder EmitPush(this ScriptBuilder sb, object obj)
+        {
+            switch (obj)
+            {
+                case bool data:
+                    sb.EmitPush(data);
+                    break;
+                case byte[] data:
+                    sb.EmitPush(data);
+                    break;
+                case string data:
+                    sb.EmitPush(data);
+                    break;
+                case BigInteger data:
+                    sb.EmitPush(data);
+                    break;
+                case sbyte data:
+                    sb.EmitPush(data);
+                    break;
+                case byte data:
+                    sb.EmitPush(data);
+                    break;
+                case short data:
+                    sb.EmitPush(data);
+                    break;
+                case ushort data:
+                    sb.EmitPush(data);
+                    break;
+                case int data:
+                    sb.EmitPush(data);
+                    break;
+                case uint data:
+                    sb.EmitPush(data);
+                    break;
+                case long data:
+                    sb.EmitPush(data);
+                    break;
+                case ulong data:
+                    sb.EmitPush(data);
+                    break;
+                case Enum data:
+                    sb.EmitPush(BigInteger.Parse(data.ToString("d")));
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+            return sb;
+        }
+
+
+        public static string ByteToHex(this byte[] data)
+        {
+            string hex = BitConverter.ToString(data).Replace("-", "");
+            return hex;
+        }
+
+        private static int BitLen(int w)
+        {
+            return (w < 1 << 15 ? (w < 1 << 7
+                ? (w < 1 << 3 ? (w < 1 << 1
+                ? (w < 1 << 0 ? (w < 0 ? 32 : 0) : 1)
+                : (w < 1 << 2 ? 2 : 3)) : (w < 1 << 5
+                ? (w < 1 << 4 ? 4 : 5)
+                : (w < 1 << 6 ? 6 : 7)))
+                : (w < 1 << 11
+                ? (w < 1 << 9 ? (w < 1 << 8 ? 8 : 9) : (w < 1 << 10 ? 10 : 11))
+                : (w < 1 << 13 ? (w < 1 << 12 ? 12 : 13) : (w < 1 << 14 ? 14 : 15)))) : (w < 1 << 23 ? (w < 1 << 19
+                ? (w < 1 << 17 ? (w < 1 << 16 ? 16 : 17) : (w < 1 << 18 ? 18 : 19))
+                : (w < 1 << 21 ? (w < 1 << 20 ? 20 : 21) : (w < 1 << 22 ? 22 : 23))) : (w < 1 << 27
+                ? (w < 1 << 25 ? (w < 1 << 24 ? 24 : 25) : (w < 1 << 26 ? 26 : 27))
+                : (w < 1 << 29 ? (w < 1 << 28 ? 28 : 29) : (w < 1 << 30 ? 30 : 31)))));
+        }
+
+        internal static int GetBitLength(this BigInteger i)
+        {
+            byte[] b = i.ToByteArray();
+            return (b.Length - 1) * 8 + BitLen(i.Sign > 0 ? b[b.Length - 1] : 255 - b[b.Length - 1]);
+        }
+
+        internal static int GetLowestSetBit(this BigInteger i)
+        {
+            if (i.Sign == 0)
+                return -1;
+            byte[] b = i.ToByteArray();
+            int w = 0;
+            while (b[w] == 0)
+                w++;
+            for (int x = 0; x < 8; x++)
+                if ((b[w] & 1 << x) > 0)
+                    return x + w * 8;
+            throw new Exception();
+        }
+
+        internal static BigInteger Mod(this BigInteger x, BigInteger y)
+        {
+            x %= y;
+            if (x.Sign < 0)
+                x += y;
+            return x;
+        }
+
+        internal static BigInteger ModInverse(this BigInteger a, BigInteger n)
+        {
+            BigInteger i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                BigInteger t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+
+        internal static BigInteger NextBigInteger(this Random rand, int sizeInBits)
+        {
+            if (sizeInBits < 0)
+                throw new ArgumentException("sizeInBits must be non-negative");
+            if (sizeInBits == 0)
+                return 0;
+            byte[] b = new byte[sizeInBits / 8 + 1];
+            rand.NextBytes(b);
+            if (sizeInBits % 8 == 0)
+                b[b.Length - 1] = 0;
+            else
+                b[b.Length - 1] &= (byte)((1 << sizeInBits % 8) - 1);
+            return new BigInteger(b);
+        }
+
+        internal static BigInteger NextBigInteger(this RandomNumberGenerator rng, int sizeInBits)
+        {
+            if (sizeInBits < 0)
+                throw new ArgumentException("sizeInBits must be non-negative");
+            if (sizeInBits == 0)
+                return 0;
+            byte[] b = new byte[sizeInBits / 8 + 1];
+            rng.GetBytes(b);
+            if (sizeInBits % 8 == 0)
+                b[b.Length - 1] = 0;
+            else
+                b[b.Length - 1] &= (byte)((1 << sizeInBits % 8) - 1);
+            return new BigInteger(b);
+        }
+
+        internal static bool TestBit(this BigInteger i, int index)
+        {
+            return (i & (BigInteger.One << index)) > BigInteger.Zero;
+        }
+
+        public static DateTime ToDateTime(this uint timestamp)
+        {
+            return unixEpoch.AddSeconds(timestamp).ToLocalTime();
+        }
+
+        public static DateTime ToDateTime(this ulong timestamp)
+        {
+            return unixEpoch.AddSeconds(timestamp).ToLocalTime();
+        }
+
+        public static string ToHexString(this IEnumerable<byte> value)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in value)
+                sb.AppendFormat("{0:x2}", b);
+            return sb.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe internal static int ToInt32(this byte[] value, int startIndex)
+        {
+            fixed (byte* pbyte = &value[startIndex])
+            {
+                return *((int*)pbyte);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe internal static long ToInt64(this byte[] value, int startIndex)
+        {
+            fixed (byte* pbyte = &value[startIndex])
+            {
+                return *((long*)pbyte);
+            }
+        }
+
+        public static uint ToTimestamp(this DateTime time)
+        {
+            return (uint)(time.ToUniversalTime() - unixEpoch).TotalSeconds;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe internal static ushort ToUInt16(this byte[] value, int startIndex)
+        {
+            fixed (byte* pbyte = &value[startIndex])
+            {
+                return *((ushort*)pbyte);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe internal static uint ToUInt32(this byte[] value, int startIndex)
+        {
+            fixed (byte* pbyte = &value[startIndex])
+            {
+                return *((uint*)pbyte);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe internal static ulong ToUInt64(this byte[] value, int startIndex)
+        {
+            fixed (byte* pbyte = &value[startIndex])
+            {
+                return *((ulong*)pbyte);
+            }
+        }
+
+        internal static long WeightedAverage<T>(this IEnumerable<T> source, Func<T, long> valueSelector, Func<T, long> weightSelector)
+        {
+            long sum_weight = 0;
+            long sum_value = 0;
+            foreach (T item in source)
+            {
+                long weight = weightSelector(item);
+                sum_weight += weight;
+                sum_value += valueSelector(item) * weight;
+            }
+            if (sum_value == 0) return 0;
+            return sum_value / sum_weight;
+        }
+
+        internal static IEnumerable<TResult> WeightedFilter<T, TResult>(this IList<T> source, double start, double end, Func<T, long> weightSelector, Func<T, long, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (start < 0 || start > 1) throw new ArgumentOutOfRangeException(nameof(start));
+            if (end < start || start + end > 1) throw new ArgumentOutOfRangeException(nameof(end));
+            if (weightSelector == null) throw new ArgumentNullException(nameof(weightSelector));
+            if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
+            if (source.Count == 0 || start == end) yield break;
+            double amount = source.Sum(weightSelector);
+            long sum = 0;
+            double current = 0;
+            foreach (T item in source)
+            {
+                if (current >= end) break;
+                long weight = weightSelector(item);
+                sum += weight;
+                double old = current;
+                current = sum / amount;
+                if (current <= start) continue;
+                if (old < start)
+                {
+                    if (current > end)
+                    {
+                        weight = (long)((end - start) * amount);
+                    }
+                    else
+                    {
+                        weight = (long)((current - start) * amount);
+                    }
+                }
+                else if (current > end)
+                {
+                    weight = (long)((end - old) * amount);
+                }
+                yield return resultSelector(item, weight);
+            }
         }
 
     }
---
+++ NEO-Emulator/VM/InteropService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Neo.VM
+{
+    public class InteropCall
+    {
+        public Func<ExecutionEngine, bool> handler;
+        public double gasCost;
+    }
+
+    public class InteropService
+    {
+        public const double defaultGasCost = 0.001;
+
+        private Dictionary<string, InteropCall> dictionary = new Dictionary<string, InteropCall>();
+
+        public InteropService()
+        {
+            Register("System.ExecutionEngine.GetScriptContainer", GetScriptContainer, defaultGasCost);
+            Register("System.ExecutionEngine.GetExecutingScriptHash", GetExecutingScriptHash, defaultGasCost);
+            Register("System.ExecutionEngine.GetCallingScriptHash", GetCallingScriptHash, defaultGasCost);
+            Register("System.ExecutionEngine.GetEntryScriptHash", GetEntryScriptHash, defaultGasCost);
+        }
+
+        public InteropCall FindCall(string method)
+        {
+            if (!dictionary.ContainsKey(method)) return null;
+            return dictionary[method];
+        }
+
+        public void Register(string method, Func<ExecutionEngine, bool> handler, double gasCost)
+        {
+            var call = new InteropCall();
+            call.handler = handler;
+            call.gasCost = gasCost;
+            dictionary[method] = call;
+        }
+
+        internal bool Invoke(string method, ExecutionEngine engine)
+        {
+            if (!dictionary.ContainsKey(method)) return false;
+            return dictionary[method].handler(engine);
+        }
+
+        private static bool GetScriptContainer(ExecutionEngine engine)
+        {
+            engine.EvaluationStack.Push(StackItem.FromInterface(engine.ScriptContainer));
+            return true;
+        }
+
+        private static bool GetExecutingScriptHash(ExecutionEngine engine)
+        {
+            engine.EvaluationStack.Push(engine.CurrentContext.ScriptHash);
+            return true;
+        }
+
+        private static bool GetCallingScriptHash(ExecutionEngine engine)
+        {
+            engine.EvaluationStack.Push(engine.CallingContext.ScriptHash);
+            return true;
+        }
+
+        private static bool GetEntryScriptHash(ExecutionEngine engine)
+        {
+            engine.EvaluationStack.Push(engine.EntryContext.ScriptHash);
+            return true;
+        }
+    }
+}
