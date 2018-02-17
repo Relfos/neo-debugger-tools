16b1a11e0f28b24b35b689d486d7c2a29c9dace8
Relfos <relfos@gmail.com>
2018-02-17 00:49:21 +0000

    Restructure of Debugger folder structure

---
+++ NEO-Debugger/Forms/RunForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Neo.Debugger
+{
+    partial class RunForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.paramsList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.assetListBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.assetAmmount = new System.Windows.Forms.TextBox();
+            this.inputGrid = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Parameter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.inputGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(350, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Debug";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(15, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // paramsList
+            // 
+            this.paramsList.FormattingEnabled = true;
+            this.paramsList.Location = new System.Drawing.Point(12, 25);
+            this.paramsList.Name = "paramsList";
+            this.paramsList.Size = new System.Drawing.Size(277, 147);
+            this.paramsList.TabIndex = 2;
+            this.paramsList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.paramsList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.paramsList_MouseDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Asset Transfer";
+            // 
+            // assetListBox
+            // 
+            this.assetListBox.FormattingEnabled = true;
+            this.assetListBox.Location = new System.Drawing.Point(295, 25);
+            this.assetListBox.Name = "assetListBox";
+            this.assetListBox.Size = new System.Drawing.Size(173, 69);
+            this.assetListBox.TabIndex = 12;
+            this.assetListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Functions";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(292, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ammount";
+            // 
+            // assetAmmount
+            // 
+            this.assetAmmount.Location = new System.Drawing.Point(295, 115);
+            this.assetAmmount.Name = "assetAmmount";
+            this.assetAmmount.Size = new System.Drawing.Size(173, 20);
+            this.assetAmmount.TabIndex = 15;
+            this.assetAmmount.Text = "0";
+            // 
+            // inputGrid
+            // 
+            this.inputGrid.AllowUserToAddRows = false;
+            this.inputGrid.AllowUserToDeleteRows = false;
+            this.inputGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inputGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Parameter,
+            this.Value});
+            this.inputGrid.Location = new System.Drawing.Point(13, 178);
+            this.inputGrid.Name = "inputGrid";
+            this.inputGrid.RowHeadersVisible = false;
+            this.inputGrid.Size = new System.Drawing.Size(455, 125);
+            this.inputGrid.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 25);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Load Private Key";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(12, 335);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(249, 13);
+            this.addressLabel.TabIndex = 18;
+            this.addressLabel.Text = "AakVz7XchUZzxbX6fAP6dA7ix6mygHm888";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Invoker Address";
+            // 
+            // Parameter
+            // 
+            this.Parameter.HeaderText = "Parameter";
+            this.Parameter.Name = "Parameter";
+            this.Parameter.ReadOnly = true;
+            this.Parameter.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // RunForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 398);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputGrid);
+            this.Controls.Add(this.assetAmmount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.assetListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.paramsList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "RunForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Invoke Smart Contract";
+            this.Shown += new System.EventHandler(this.RunForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.inputGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox paramsList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox assetListBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox assetAmmount;
+        private System.Windows.Forms.DataGridView inputGrid;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Parameter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
---
+++ NEO-Debugger/Forms/RunForm.cs
@@ -0,0 +1,259 @@
+﻿using LunarParser;
+using LunarParser.JSON;
+using Neo.Debugger.Utils;
+using Neo.Emulator;
+using Neo.Emulator.API;
+using Neo.Emulator.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace Neo.Debugger
+{
+    public partial class RunForm : Form
+    {
+        public NeoEmulator emulator;
+        public ABI abi;
+
+        private string lastParams = null;
+
+        public RunForm()
+        {
+            InitializeComponent();
+
+            assetListBox.Items.Clear();
+            assetListBox.Items.Add("None");
+            foreach (var entry in Asset.Entries)
+            {
+                assetListBox.Items.Add(entry.name);
+            }
+            assetListBox.SelectedIndex = 0;
+        }
+
+        private Dictionary<string, string> _lastParams = new Dictionary<string, string>();
+
+        private void LoadFunction(string key)
+        {
+            if (abi.functions.ContainsKey(key))
+            {
+                var f = abi.functions[key];
+
+                inputGrid.Rows.Clear();
+
+                if (f.inputs != null)
+                {
+                    foreach (var p in f.inputs)
+                    {
+                        var temp = (key + "_" + f.name).ToLower();
+                        string val = "";
+
+                        if (_lastParams.ContainsKey(temp))
+                        {
+                            val = _lastParams[temp];
+                        }
+
+                        inputGrid.Rows.Add(new object[] { p.name, val});
+                    }
+                }
+
+                lastParams = key;
+
+                button1.Enabled = true;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var key = paramsList.Text;
+            LoadFunction(key);
+        }
+
+        private bool InitInvoke()
+        {
+
+            var key = paramsList.Text;
+            var f = abi.functions[key];
+
+            var argList = "";
+
+            if (f.inputs != null)
+            {
+                int index = 0;
+                foreach (var p in f.inputs)
+                {
+                    var temp = ($"{key}_{f.name}").ToLower();
+                    var val = inputGrid.Rows[index].Cells[1].Value;
+
+                    if (index>0)
+                    {
+                        argList += ",";
+                    }
+
+                    switch (p.type.ToLower())
+                    {
+                        case "string": val = $"\"{val}\""; break;
+                    }
+
+                    argList += val;
+                    index++;
+                }
+            }
+
+            if (key != abi.entryPoint.name)
+            {
+                if (f.inputs == null || f.inputs.Length == 0)
+                {
+                    argList = "[null]";
+                }
+                argList = $"\"{key.ToLowerInvariant()}\", {argList}";
+            }
+
+            string json = "{\"params\": ["+ argList + "]}";
+
+            if (string.IsNullOrEmpty(json))
+            {
+                MessageBox.Show("Invalid input!");
+                return false;
+            }
+
+            DataNode node;
+
+            try
+            {
+                node = JSONReader.ReadFromString(json);
+            }
+            catch
+            {
+                MessageBox.Show("Error parsing input!");
+                return false;
+            }
+
+            var items = node.GetNode("params");
+
+            if (assetListBox.SelectedIndex > 0)
+            {
+                foreach (var entry in Asset.Entries)
+                {
+                    if (entry.name == assetListBox.SelectedItem.ToString())
+                    {
+                        BigInteger ammount;
+
+                        BigInteger.TryParse(assetAmmount.Text, out ammount);
+
+                        if (ammount > 0)
+                        {
+                            emulator.SetTransaction(entry.id, ammount);
+                        }
+                        else
+                        {
+                            MessageBox.Show(entry.name + " ammount must be greater than zero");
+                            return false;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            emulator.Reset(items);
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (InitInvoke())
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void RunForm_Shown(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.None;
+
+            inputGrid.AllowUserToAddRows = false;
+
+            assetAmmount.Enabled = assetListBox.SelectedIndex > 0;
+
+            if (Runtime.invokerKeys == null && File.Exists("last.key"))
+            {
+                var privKey = File.ReadAllBytes("last.key");
+                if (privKey.Length == 32)
+                {
+                    Runtime.invokerKeys = new NeoLux.KeyPair(privKey);
+                }
+            }
+
+            if (Runtime.invokerKeys != null)
+            {
+                addressLabel.Text = Runtime.invokerKeys.address;
+            }
+            else
+            {
+                addressLabel.Text = "(No key loaded)";
+            }
+
+            paramsList.Items.Clear();
+
+            foreach (var f in abi.functions.Values)
+            {
+                paramsList.Items.Add(f.name);
+            }
+
+            int mainItem = paramsList.FindString("Main");
+            if (mainItem >= 0) paramsList.SetSelected(mainItem, true);
+
+            /*if (_paramMap != null)
+            {
+                foreach (var entry in _paramMap)
+                {
+                    paramsList.Items.Add(entry.Key);
+
+                    if (lastParams != null && entry.Key.Equals(lastParams))
+                    {
+                        paramsList.SelectedIndex = paramsList.Items.Count - 1;
+                    }
+                }
+
+                if (paramsList.SelectedIndex<0 && paramsList.Items.Count > 0)
+                {
+                    paramsList.SelectedIndex = 0;
+                }
+
+            }*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string input = "";
+            if (InputUtils.ShowInputDialog("Enter private key", ref input) == DialogResult.OK)
+            {
+                var privKey = input.HexToByte();
+                if (privKey.Length == 32)
+                {
+                    Runtime.invokerKeys = new NeoLux.KeyPair(privKey);
+                    addressLabel.Text = Runtime.invokerKeys.address;
+
+                    File.WriteAllBytes("last.key", privKey);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid private key, length should be 32");
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            assetAmmount.Enabled = assetListBox.SelectedIndex > 0;
+        }
+
+        private void paramsList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            button1_Click(null, null);
+        }
+    }
+}
