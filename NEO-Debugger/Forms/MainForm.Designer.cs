3220a634567c5210b5cbca33f45df535c680ad8c
gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2018-02-28 17:27:42 -0500

    Cleanup

--- NEO-Debugger/Forms/MainForm.Designer.cs
+++ NEO-Debugger/Forms/MainForm.Designer.cs
@@ -514,7 +514,6 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
-            this.Text = "v";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.Resize += new System.EventHandler(this.MainForm_Resize);
